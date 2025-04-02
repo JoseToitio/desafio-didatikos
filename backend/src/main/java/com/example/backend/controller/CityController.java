d7303e5e6659ed54ad5140f1542e8e8fc9991675
JoseToitio <josetoitio@gmail.com>
2025-04-02 03:34:20 -0300

    feat: add city controller

---
+++ backend/src/main/java/com/example/backend/controller/CityController.java
@@ -0,0 +1,28 @@
+package com.example.backend.controller;
+
+import com.example.backend.dto.CreateCityDto;
+import com.example.backend.entitities.City;
+import com.example.backend.services.CityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cidades")
+public class CityController {
+    private final CityService cityService;
+    public CityController(CityService cityService) {
+        this.cityService = cityService;
+    }
+
+    @PostMapping
+    public ResponseEntity<City> create(@RequestBody CreateCityDto dto) {
+        return ResponseEntity.ok(cityService.createCity(dto));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<City> get(@PathVariable Long id) {
+        return ResponseEntity.ok(cityService.findById(id));
+    }
+}
