3dacc01cfca56ef1ad406b569e2ff386a71c7942
JoseToitio <josetoitio@gmail.com>
2025-04-03 23:04:23 -0300

    chore: auth guard user login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ProductComponent } from './pages/product/product.component';
 import { EditProductComponent } from './pages/edit-product/edit-product.component';
 import { CreateProductComponent } from './pages/create-product/create-product.component';
+import { AuthGuard } from './services/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'products', component: ProductComponent },
-  { path: 'edit-product/:id', component: EditProductComponent },
+  { path: 'edit-product/:id', component: EditProductComponent, canActivate: [AuthGuard] },
   { path: 'create-product', component: CreateProductComponent }
 ];
 
