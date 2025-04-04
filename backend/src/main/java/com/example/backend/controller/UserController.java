46e3fd32ba9128390b3b3adfd3545fff38f0b608
JoseToitio <josetoitio@gmail.com>
2025-04-03 21:53:28 -0300

    feat: new features userController

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -5,20 +5,30 @@ import com.example.backend.dto.CreateUserDto;
 import com.example.backend.dto.LoginDto;
 import com.example.backend.dto.UserResponseDto;
 import com.example.backend.entitities.User;
+import com.example.backend.exceptions.UserException;
+import com.example.backend.repositories.UserRepository;
+import com.example.backend.security.JwtTokenService;
 import com.example.backend.services.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
+    private final JwtTokenService jwtTokenService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserRepository userRepository, JwtTokenService jwtTokenService) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.jwtTokenService = jwtTokenService;
     }
 
     //register user
@@ -29,8 +39,9 @@ public class UserController {
 
     //login user
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDto dto) {
-        return ResponseEntity.ok(userService.authenticate(dto));
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginDto dto) {
+        String token = userService.authenticate(dto);
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
     }
 
     @GetMapping
@@ -53,4 +64,18 @@ public class UserController {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserResponseDto> getUserInfo(@RequestHeader("Authorization") String token) {
+        try {
+            String cpf = jwtTokenService.getCpfFromToken(token.replace("Bearer ", ""));
+            User user = userRepository.findByCpf(cpf)
+                    .orElseThrow(() -> new UserException("Usuário não encontrado"));
+            UserResponseDto userDto = new UserResponseDto(user.getId(), user.getName(), user.getCpf(), user.getRole());
+            return ResponseEntity.ok(userDto);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
+
 }
